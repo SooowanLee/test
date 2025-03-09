851bd2d8f7781599ec7b1892324afd13a506dc94
Lee Soowan <lsy9406@gmail.com>
2025-03-09 17:09:04 +0900

    a.txt 생성

--- src/main/java/com/example/member/repository/MemberRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.member.repository;
-
-import com.example.member.entity.MemberEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-    MemberEntity findByMemberEmail(String memberEmail);
-}
